1e1e97d5a4f968150ec718a8b48a2b2cfde118d2
Rodriguez <0230619@students.dadeschools.net>
2019-04-16 10:16:42 -0400

    Started project

---
+++ main.js
@@ -0,0 +1,16 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+
+// Question set
+var questions = [
+
+    {
+    question: "What was the first full length CGI movie?",
+    answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+    correctAnswer: "Toy Story",
+    image: ""
+    },
+
+]
